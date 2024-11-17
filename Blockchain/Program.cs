9b388a0cc46d04c5faae9a167c060383d51853ed
Spark <sparkismad@gmail.com>
2024-11-17 20:47:25 +0100

    Add sources

---
+++ Blockchain/Program.cs
@@ -0,0 +1,13 @@
+﻿using NBitcoin;
+
+namespace Blockchain;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Key privateKey = new Key();
+        PubKey publicKey = privateKey.PubKey;
+        Console.WriteLine(publicKey);
+    }
+}
